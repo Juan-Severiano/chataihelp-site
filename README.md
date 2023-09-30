769777addfe190016341cf1f4db08adeb06e6267
juansev <juansoussev@gmail.com>
2023-09-30 19:00:27 -0300

    REFACTOR: Firsts configurations

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
-# React + Vite
+# Chat AI.Help
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
-
-Currently, two official plugins are available:
-
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+O
+projeto ChatAI.help foi pensado e desenvolvido com o intuito de orientar
+às vítimas de discriminações étnico-raciais por meio de uma conversa com uma
+inteligência artificial que direcionaria a mesma na tomada de decisões sobre a
+situação ocorrida, propondo ao indivíduo informações mais aprofundadas sobre
+determinado fato, e orientando-lhe no que poderá ser feito após o incidente
+para que as medidas cabíveis possam ser tomadas, usando como meio de
+comunicação um aplicativo que proverá o diálogo entre a vítima e a inteligência
+artificial, utilizando tecnologias modernas e eficazes.
--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,12 @@
-import { useState } from 'react'
-import './App.css'
-import OpenAI from 'openai';
+import { Component } from "react";
+import './styles/styles.css'
 
-const openai = new OpenAI({ apiKey: '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' });
-
-function App() {
-  const [count, setCount] = useState(0)
-
-
-  const main = async () => {
-    const completion = await openai.completions.create({
-      model: "gpt-3.5-turbo-instruct",
-      prompt: "Say this is a test.",
-      max_tokens: 7,
-      temperature: 0,
-    });
-
-    console.log(completion);
-    console.log(completion.choices[0].text);
+export default class App extends Component {
+  render() {
+    return  (
+      <header className="container">
+        <h1>Hello world</h1>
+      </header>
+    )
   }
-  return (
-    <>
-      <div>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => main()}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
 }
-
-export default App
